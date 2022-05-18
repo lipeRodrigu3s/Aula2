e2ecfa5144981a8718f897192135227623f84839
Fellipe Augusto <Fellipe246@hotmail.com>
2022-05-18 12:36:38 -0300

    Aula2

--- src/Aula2/Principal.java
+++ src/Aula2/Principal.java
@@ -1,19 +1,18 @@
 package Aula2;
-
 public class Principal {
-    public static void main(String[] args){
+  public static void main(String[] args){
 
-        double areaQuadrado = Quadrilatero.area(5);
+      double areaQuadrado = Quadrilatero.area(5);
 
-            System.out.println("Quadrado: " + areaQuadrado);
+      System.out.println("Quadrado: " + areaQuadrado);
 
         double areaRetangulo = Quadrilatero.area(5,7);
 
-            System.out.println("retângulo: "+ areaRetangulo);
+        System.out.println("retângulo: "+ areaRetangulo);
 
         double areaTrapezio = Quadrilatero.area(5,7,9);
 
-            System.out.println("Trapezio: " + areaTrapezio);
+        System.out.println("Trapezio: " + areaTrapezio);
 
     }
 }
--- src/Aula2/Quadrilatero.java
+++ src/Aula2/Quadrilatero.java
@@ -4,11 +4,11 @@ public class Quadrilatero {
 
     public static double area(double lado){
 
-        return lado + lado;
+       return lado + lado;
 
     }
 
-    public static double area(double lado1, double lado2){
+   public static double area(double lado1, double lado2){
 
         return lado1 + lado2;
 
@@ -17,5 +17,5 @@ public class Quadrilatero {
     public static double area(double baseMaior, double baseMenor, double altura){
 
         return ((baseMaior + baseMenor)*altura)/2;
-    }
+   }
 }
